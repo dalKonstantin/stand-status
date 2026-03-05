9b83b562095d3347db91729689394c0409acf79a
Константин Даль <djkostjan@MacBook-Pro-Konstantin.local>
2026-03-05 13:33:11 +0300

    upd: simple pinger with tests

--- src/network.rs
+++ src/network.rs
@@ -1,3 +1,27 @@
-use std::net::Ipv4Addr;
+use std::{net::Ipv4Addr, process::Command};
 
-pub fn ping_host(host: Ipv4Addr) {}
+pub fn ping_host(host: Ipv4Addr) -> bool {
+    let host_string = host.to_string();
+    let mut child = Command::new("ping")
+        .args(["-c", "1", "-W", "1", &host_string])
+        .status()
+        .expect("Failed to execute ping");
+    if child.success() { true } else { false }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ping_eights() {
+        let res = ping_host(Ipv4Addr::new(8, 8, 8, 8));
+        assert_eq!(res, true);
+    }
+
+    #[test]
+    fn ping_wrong_ip() {
+        let res = ping_host(Ipv4Addr::new(88, 88, 8, 88));
+        assert_eq!(res, false);
+    }
+}
